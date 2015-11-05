97a9956f79d5aba19e658097c87ef25316bae36e
jwanner <jacobwanner1@gmail.com>
2015-11-05 10:37:13 -0500

    limit input to one letter at a time.  Started limbs method, added comments for ideas on incrementing live (or limbs) remaining.

--- hangman.rb
+++ hangman.rb
@@ -1,24 +1,27 @@
-
+require 'goto'
 prompt = "> "
 
-letters = ['a','b','c','d','e','f','g','h','i','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+letters = ['a','b','c','d','e','f','g','h','i','i','j','k','l',
+	'm','n','o','p','q','r','s','t','u','v','w','x','y','z']
 
 puts "What is your word?", prompt
 
 secret_word = $stdin.gets.chomp
+
 geuss_letter ="Geuss a letter"
-puts geuss_letter, prompt
-geuss = $stdin.gets.chomp.downcase
 
-
-#issues
-if geuss.length > 1
-	puts "You must only enter 1 letter"
-	puts geuss_letter
-	guess = $stdin.gets.chomp.downcase
-elsif geuss.count(letters) == 0 
-	puts "You must enter a letter!",prompt
+puts geuss_letter, prompt	
+while true
+	if gets.chomp.length == 1
+		puts "correct"		
+		break
+	end		
+	puts "You can only geuss 1 letter at a time, try again"
 end
+
+
+
+
 
 
 def limbs()
@@ -30,5 +33,9 @@ def limbs()
 
 	end
 end
+
+# ideas for keeping track of number of limbs remaining, 
+# using an incremting += and check each time if it is 
+# less than x
 
 
