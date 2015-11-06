1eb62119f04907be1e62ba62c28de36412ef7a4f
jwanner <jacobwanner1@gmail.com>
2015-11-06 12:28:52 -0500

    method to check if letter is in secret word works

--- hangman.rb
+++ hangman.rb
@@ -5,16 +5,17 @@ letters = ['a','b','c','d','e','f','g','h','i','i','j','k','l',
 
 puts "What is your word?", prompt
 
-secret_word = $stdin.gets.chomp
+$secret_word = $stdin.gets.chomp
 
 geuss_letter ="Geuss a letter"
 fail_counter = 0
 
 # ensure user only geusses one letter at a time
-puts geuss_letter, prompt	
+puts geuss_letter, prompt
+letter_geuss = $stdin.gets.chomp 	
 while true
-	if gets.chomp.length == 1
-		puts "correct"		
+	if letter_geuss.length == 1
+        valid_geuss = letter_geuss 		
 		break
 	end		
 	puts "You can only geuss 1 letter at a time, try again \n> "
@@ -25,21 +26,20 @@ while true
 	end
 end
 
- 
 
 
-
-
-def limbs()
-	if secret_word.count(geuss) >= 1
-		puts "Correct! #{letter_geuss} is in this word 
-		#{secret_word.count(letter_geuss)} times"
+# check to see if geuss is in the secret word
+def limbs(geuss)
+	if $secret_word.count(geuss) >= 1
+		puts "Correct! #{geuss} is in this word 
+		#{$secret_word.count(geuss)} times"
 	else
 		puts "Nope! You loose a limb" 
 
 	end
 end
 
+puts limbs(valid_geuss)
 # ideas for keeping track of number of limbs remaining, 
 # using an incremting += and check each time if it is 
 # less than x
